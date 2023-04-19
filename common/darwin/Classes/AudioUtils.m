7105fa87c0818fb5a233a5430372cbd6810298cc
Jovan Stojanov <railjoe@gmail.com>
2023-04-19 02:19:13 +0200

    Update AudioUtils.m

--- common/darwin/Classes/AudioUtils.m
+++ common/darwin/Classes/AudioUtils.m
@@ -15,7 +15,7 @@
     config.category = AVAudioSessionCategoryPlayAndRecord;
     config.categoryOptions =
         AVAudioSessionCategoryOptionAllowBluetooth | AVAudioSessionCategoryOptionAllowBluetoothA2DP;
-    config.mode = AVAudioSessionModeVideoChat;
+    //config.mode = AVAudioSessionModeVideoChat;
 
     [session lockForConfiguration];
     [session setCategory:config.category withOptions:config.categoryOptions error:nil];
